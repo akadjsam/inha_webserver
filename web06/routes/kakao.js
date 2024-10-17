a2985c7fbc158a51ba4e475399de79d29d09247b
akadjsam <akadjsam@gmail.com>
2024-10-17 22:48:42 +0900

    fix week06

--- web06/routes/kakao.js
+++ web06/routes/kakao.js
@@ -23,8 +23,9 @@ router.get('/book/info', function(req, res, next) {
   const authors = req.query.authors;
   const publisher = req.query.publisher;
   const isbn = req.query.isbn;
+  const contents = req.query.contents;
   console.log("title", title);
-  const info = {title, thumbnail,price,authors,publisher,isbn};
+  const info = {title, thumbnail,price,authors,publisher,isbn, contents};
   res.render('index', { title: '도서정보', pageName:'kakao/info.ejs', info});
 });
 
